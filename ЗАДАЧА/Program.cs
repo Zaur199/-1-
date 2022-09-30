b14335e3506edc6f63a88f620be22a9be0c45213
Zaur199 <zaur.rojer@mail.ru>
2022-09-30 11:31:16 +0300

    добавили описание задачи

---
+++ ЗАДАЧА/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {1, 2, 3, 9, -8, 6, 0, -4};
+Console.Write("Текущий массив:");
+
+ for(int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+
+Console.Write("Преобразованный массив:");
+for(int i = 0; i < array.Length - 5; i++)
+{
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
